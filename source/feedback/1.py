93a713093f708673371173af9d2f4febd913d1ea
Ludovico Caldara <ludovico.caldara@gmail.com>
2023-07-17 15:36:16 +0000

    feedback 1: run 1.py

---
+++ source/feedback/1.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append('..')
+
+import MWMusicalArtist
+from MWMusicalArtist import NoMusicalInfoboxException
+from MWMusicalArtist import RedirectException
+
+import json
+
+ret = MWMusicalArtist.MWMusicalArtist('Kyuss').getDict()
+
+print(json.dumps(ret, indent=4))
